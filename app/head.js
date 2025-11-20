df38b1c1d0ebd5eb2ce704236cb5955a6c28a570
savithaprofile <savitha.saviy@gmail.com>
2025-11-20 23:46:26 +0530

    verify

--- app/head.js
+++ app/head.js
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <meta name="google-site-verification" content="yGAGClhKmqd-q61BmWlHPO3GsYzBEE2JZbOPhPIbdtY" />
+
       <meta name="google-site-verification" content="WEXErXa5JBg5hZPCEKFY_g1UVf9R3AxHCZYgQWjQspY" />
       <link rel="icon" href="/favicon.ico" />
       {/* other global head tags */}
