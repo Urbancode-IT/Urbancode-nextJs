66171b0fcfb7bf275344ee78ea5f2bbe9776d470
jackroshan-y <jakc254003@gmail.com>
2026-01-19 11:55:49 +0530

    chatbot

--- app/components/ChatbotWidget.jsx
+++ app/components/ChatbotWidget.jsx
@@ -76,7 +76,7 @@ const ChatbotWidget = () => {
             }}
           >
             <div className="chatbot-text">
-              🎉 Festive Offers are Live! <br/>Enroll a course today
+              🎉Exciting Offers are Live! <br/>Enroll a course today
             </div><br/>
             
           </div>
