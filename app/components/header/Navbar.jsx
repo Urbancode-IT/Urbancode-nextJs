5c186d5c35dfcd8696e45b3719f0da7ab17e1582
Abinash-003 <abinash220304@gmail.com>
2026-02-07 12:02:43 +0530

    Revert "navbar altered"

--- app/components/header/Navbar.jsx
+++ app/components/header/Navbar.jsx
@@ -33,7 +33,7 @@ export default function Navbar() {
   return (
     <>
       <nav className={`navbar ${isOpen ? 'menu-open' : ''}`}>
-        <div className="container">
+        <div className="navbar container">
 
           {/* Logo */}
           <Link href="/" className="navbar-brand" onClick={handleLinkClick}>
