61a3fd795ce0285a12bc7dfd6603165eee0141f7
savithaprofile <savitha.saviy@gmail.com>
2026-02-26 19:58:20 +0530

    cors

--- compiler/compiler/backend/server.js
+++ compiler/compiler/backend/server.js
@@ -26,6 +26,7 @@ const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:5174',
     'http://localhost:5175',
+    'https://www.urbancode.in/',
     process.env.CLIENT_URL
 ].filter(Boolean);
 
